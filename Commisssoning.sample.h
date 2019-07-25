c0107e1b59734c55060abe26dde98cfe65e0cff8
Guiguitant Théo <theo.guiguitant@gmail.com>
2019-07-25 16:30:42 +0200

    change commissioning.h

--- Commisssoning.sample.h
+++ Commisssoning.sample.h
@@ -2,22 +2,15 @@
 /*OTAA->1
  *ABP->0
  */
-#define DutyCycle     1 // respect ou non des 1% de transmission du LoRa uniquement pour les teste (par défaut 1)
-#define PublicNetwork 1 // 1 par défaut
-#define DataRate      0// vitesse d'envoie 
-/*faible vitesse d'envoie
- * => long portée et temps de transmission plus long 
- * donc moins de données envoyer pendant 1% du temps
- * et consommation importante
- * equivalent à 0
- * forte vtesse d'envoie 
- * => portée plus court et temps de transmission plus rapide donc   
- * plus facile de respecter le data rate 
- * et consommation plus faible
- * equivalent à 5
-  */
-#define ADR           1  //adaptative data rate(par défaut 1)
-/*reglage capaciter max(mais interdit)
+#define DutyCycle     1 // sending 1% of time ( 1 by default)
+#define PublicNetwork 1 // 1 by default
+#define DataRate      0// sending speed
+/*
+0- long range and low sending speed
+5- small range and high sending speed
+*/
+#define ADR           1  //adaptative data rate(1 by default)
+/*set max capacity to long range 
  * 0 0 0 0
  */
 /*OTAA connection adresse*/
